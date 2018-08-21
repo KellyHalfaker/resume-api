837171b2ac141345216f7121fd993dfb4a2f9a09
Jessica Hernandez <jessdc04@gmail.com>
2018-08-20 19:30:55 -0500

    Changed student table to students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_001148) do
+ActiveRecord::Schema.define(version: 2018_08_21_002914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2018_08_21_001148) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "student", force: :cascade do |t|
+  create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
