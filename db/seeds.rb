82cf9a3d46ed45c3fd1fe1ddfe34c1897158b336
Kelly Halfaker <halfakerkelly@gmail.com>
2018-08-20 20:42:27 -0500

    corrected variable names

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@ student2.save
 student3 =  Student.new({first_name: "Tim", last_name: "Long", email: "tim@email.com", phone_number: "333-333-3333", short_bio: "My name is tim and I would like a job.", linkedin_url: "tim@linkedin.com", twitter_handle: "@tim", personal_blog: "medium.com/tim", online_resume_url: "resume.com/tim", github_url: "github.com/tim", photo: "photo.com/tim"})
 student3.save
 
-experience1 = Experience.new({start_date: Date.now - 10.days, end_date: Date.now - 2.days, job_title: "Very important dude", company_name: "Important People Inc.", details: "I was a very important dude"})
+experience1 = Experience.new({start_date: Date.today - 10.days, end_date: Date.today - 2.days, job_title: "Very important dude", company_name: "Important People Inc.", details: "I was a very important dude"})
 experience1.save
-experience2 = Experience.new({start_date: Date.now - 15.days, end_date: Date.now - 1.days, job_title: "Cool gal", company_name: "Yoyoyoyoyo", details: "Very chill"})
+experience2 = Experience.new({start_date: Date.today - 15.days, end_date: Date.today - 1.days, job_title: "Cool gal", company_name: "Yoyoyoyoyo", details: "Very chill"})
 experience2.save
-experience3 = Experience.new({start_date: Date.now - 20.days, end_date: Date.now - 1.days, job_title: "Wow", company_name: "Great", details: "Excellent"})
+experience3 = Experience.new({start_date: Date.today - 20.days, end_date: Date.today - 1.days, job_title: "Wow", company_name: "Great", details: "Excellent"})
 experience3.save
 
 skill1 = Skill.new({Name: "Metalworking"})
@@ -29,10 +29,10 @@ skill3 = Skill.new({Name: "Turning technology off and back on again to fix it"})
 skill3.save
 
 
-education1 = Education.new({start_date: Date.now - 300.days, end_date: Date.now - 100.days, degree: "Bachelors in Partying", University_name: "Harvard", details: "I was hecka good at partying"})
+education1 = Education.new({start_date: Date.today - 300.days, end_date: Date.today - 100.days, degree: "Bachelors in Partying", University_name: "Harvard", Details: "I was hecka good at partying"})
 education1.save
-education2 = Education.new({start_date: Date.now - 500.days, end_date: Date.now - 200.days, degree: "Bachelors in Napping", University_name: "Duke", details: "
+education2 = Education.new({start_date: Date.today - 500.days, end_date: Date.today - 200.days, degree: "Bachelors in Napping", University_name: "Duke", Details: "
   Sleep is important"})
 education2.save
-education3 = Education.new({start_date: Date.now - 900.days, end_date: Date.now - 300.days, degree: "Bachelors in Napping", University_name: "MIT", details: "I need 10 hours a night at least"})
+education3 = Education.new({start_date: Date.today - 900.days, end_date: Date.today - 300.days, degree: "Bachelors in Napping", University_name: "MIT", Details: "I need 10 hours a night at least"})
 education3.save
