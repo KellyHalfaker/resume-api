141642fb68f3a1d91331139a488c6665da240b25
“Vishnu <vvichugit@gmail.com>
2018-08-20 21:21:58 -0500

    added the student Id

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -10,7 +10,9 @@ class ExperiencesController < ApplicationController
       end_date: params[:end_date],
       job_title: params[:job_title],
       company_name: params[:company_name],
-      details: params[:details]
+      details: params[:details],
+      student_id: params[:student_id]
+
     )
     @experience.save
     render "show.json.jbuilder"
@@ -24,6 +26,9 @@ class ExperiencesController < ApplicationController
     @experience.job_title = params[:job_title] || @experience.job_title
     @experience.company_name = params[:company_name] || @experience.company_name
     @experience.details = params[:details] || @experience.details
+    @experience.student_id = params[:student_id] || @experience.student_id
+    
+
  
     if @experience.save
       render "show.json.jbuilder"
--- app/views/experiences/show.json.jbuilder
+++ app/views/experiences/show.json.jbuilder
@@ -3,4 +3,5 @@ json.end_date @experience.end_date
 json.job_title @experience.job_title
 json.company_name @experience.company_name
 json.details @experience.details
+json.student_id @experience.student_id
 
