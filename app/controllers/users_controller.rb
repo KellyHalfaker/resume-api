369d2a7fae89cdd0fa29255cfbf1177c8a1b7b5d
Kelly Halfaker <halfakerkelly@gmail.com>
2018-08-21 20:48:20 -0500

    symbol removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController=
+class UsersController < ApplicationController
   def show_current_user
     @student = Student.find_by(id: current_user.id)
     render "show_current_user.json.jbuilder"
