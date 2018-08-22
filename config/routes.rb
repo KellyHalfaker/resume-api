b85c237f3b8062f0435aa7d62d843588ea357652
Jessica Hernandez <jessdc04@gmail.com>
2018-08-21 20:07:11 -0500

    Created sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/users" => "users#create"
-   
+
+  post "/sessions" => "sessions#create"
+
   get "/students" => "students#index"
 
   get "/students/:id" => "students#show"
